39560090ec00317b24ab711094b326cb91e4c186
Ashwin <ashwin.shiv@icloud.com>
2022-02-24 14:05:28 -0800

    added CrossOrigin to avoid front end from being blocked by CORS policy

--- src/main/java/com/ashwin/employee/controller/EmployeeController.java
+++ src/main/java/com/ashwin/employee/controller/EmployeeController.java
@@ -4,11 +4,13 @@ import com.ashwin.employee.model.EmployeeModel;
 import com.ashwin.employee.services.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
